d91bd054586d111b9bd50ef4e0e55e59db849d0f
Yair Barnatan <59892775+ybarnatan@users.noreply.github.com>
2025-11-30 19:32:37 -0300

    vuelvo el proyecto a su estado original

--- src_feateng/feat_eng_armado_tablas.py
+++ src_feateng/feat_eng_armado_tablas.py
@@ -6,7 +6,6 @@ from src.config import *
 from src.configuracion_inicial import creacion_df_small
 from src.constr_lista_cols import contruccion_cols,cols_a_dropear_variable_entera,cols_a_dropear_variable_por_feat ,cols_a_dropear_variable_originales_o_percentiles,cols_a_dropear_variable_originales_o_corregidas ,cols_conteo_servicios_productos,cols_beneficios_presion_economica
 from src.feature_engineering import feature_engineering_lag_orden_fijo,feature_engineering_delta_orden_fijo,feature_engineering_mean,feature_engineering_mes,feature_engineering_ctrx_norm,feature_engineering_mpayroll_sobre_edad,conversion_parquet,copia_tabla,feature_engineering_correccion_variables_por_mes_por_media,suma_de_prod_servs,suma_ganancias_gastos,ratios_ganancia_gastos,feature_engineering_percentil,feature_engineering_lag,feature_engineering_delta,feature_engineering_max_min,feature_engineering_ratio,feature_engineering_linreg,feature_engineering_drop_cols,feature_engineering_drop_meses
-from src_feateng.feature_engineering_YB import *
 ## ---------------------------------------------------------Configuraciones Iniciales -------------------------------
 ## Carga de variables
 
@@ -54,10 +53,6 @@ def lanzar_feat_eng(fecha:str ,n_fe:int , proceso_ppal:str):
     _,_,cols_ratios = contruccion_cols(df_completo_chiquito)
     feature_engineering_ratio(df_completo_chiquito,cols_ratios)
  
-
-    # #Adicional Exp 800  FE Mia
-    df_completo_chiquito=creacion_df_small("df_completo")
-    ejecutar_todo_Feat_Eng_YB(df_completo_chiquito, VENTANA)
 
 
     df_completo_chiquito=creacion_df_small("df_completo")
